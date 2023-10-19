d901826bbdb3579da5fe0a99d6f470bc18ea9200
palango <arean-part.alango@ut.ee>
2023-10-19 15:34:48 +0300

    [Task 1 complete] 12-hour format clock added

--- script.js
+++ script.js
@@ -1,104 +1,94 @@
-(function() {
-    "use strict";
-    
-    //clock
+(function () {
+  "use strict";
 
-    document.addEventListener("DOMContentLoaded", function() {
-        
-        let c = document.getElementById("clock");
-       
-        //setTimeout(updateClock, 2000);
-        setInterval(updateClock, 1000);
-        
-        function updateClock() {
-            
-            let date = new Date();
-            let h = date.getHours();
-            let m = date.getMinutes();
-            let s = date.getSeconds();
+  //clock
 
-            if (h < 10) {
-                h = "0" + h;
-            }
+  document.addEventListener("DOMContentLoaded", function () {
+    let c = document.getElementById("clock");
 
-            if (m < 10) {
-                m = "0" + m;
-            }
+    //setTimeout(updateClock, 2000);
+    setInterval(updateClock, 1000);
 
-            if (s < 10) {
-                s = "0" + s;
-            }
+    function updateClock() {
+      let date = new Date();
+      let h = date.getHours();
+      let m = date.getMinutes();
+      let s = date.getSeconds();
+      let suffix = "EL";
+      if (h > 12) {
+        suffix = "PL";
+        h -= 12;
+      }
 
-            c.innerHTML = h + ":" + m + ":" + s;
-            
-        };
-        
-    });
-    
-    // forms
-    
-    document.getElementById("form").addEventListener("submit", estimateDelivery);
-    
-    let e = document.getElementById("delivery");
-    e.innerHTML = "0,00 &euro;";
-    
-    function estimateDelivery(event) {
-        event.preventDefault();
-        
-        let linn = document.getElementById("linn");
-        
-        if (linn.value === "") {
-            
-            alert("Palun valige linn nimekirjast");
-            
-            linn.focus();
-            
-            return;
-            
-            
-        } else {
-            
-            e.innerHTML = "x,xx &euro;";
-            
-        }        
-        
-        console.log("Tarne hind on arvutatud");
+      if (h < 10) {
+        h = "0" + h;
+      }
+
+      if (m < 10) {
+        m = "0" + m;
+      }
+
+      if (s < 10) {
+        s = "0" + s;
+      }
+
+      c.innerHTML = h + ":" + m + ":" + s + " " + suffix;
     }
-    
+  });
+
+  // forms
+
+  document.getElementById("form").addEventListener("submit", estimateDelivery);
+
+  let e = document.getElementById("delivery");
+  e.innerHTML = "0,00 &euro;";
+
+  function estimateDelivery(event) {
+    event.preventDefault();
+
+    let linn = document.getElementById("linn");
+
+    if (linn.value === "") {
+      alert("Palun valige linn nimekirjast");
+
+      linn.focus();
+
+      return;
+    } else {
+      e.innerHTML = "x,xx &euro;";
+    }
+
+    console.log("Tarne hind on arvutatud");
+  }
 })();
 
 // map
 
-let mapAPIKey = "AqLLRE37SJGqIxXEYxezPUa6fF2oCzl3cvG4n05FtFIVBrotBYxchpMYYpwuxBak";
+let mapAPIKey =
+  "Amu--4cP4XlBj-lWqCopGmNdG-Bvjvc1VFNO-pMHjJevdMKQXYglqNKlUYyKabjJ";
 
 let map;
 
 function GetMap() {
-    
-    "use strict";
+  "use strict";
 
-    let centerPoint = new Microsoft.Maps.Location(
-            58.38104, 
-            26.71992
-        );
+  let centerPoint = new Microsoft.Maps.Location(58.38104, 26.71992);
 
-    map = new Microsoft.Maps.Map("#map", {
-        credentials: mapAPIKey,
-        center: centerPoint,
-        zoom: 14,
-        mapTypeId: Microsoft.Maps.MapTypeId.road,
-        disablePanning: true
-    });
-    
-    let pushpin = new Microsoft.Maps.Pushpin(centerPoint, {
-            title: 'Tartu Ülikool',
-            //subTitle: 'Hea koht',
-            //text: 'UT'
-        });
+  map = new Microsoft.Maps.Map("#map", {
+    credentials: mapAPIKey,
+    center: centerPoint,
+    zoom: 14,
+    mapTypeId: Microsoft.Maps.MapTypeId.road,
+    disablePanning: true,
+  });
 
-    map.entities.push(pushpin);
+  let pushpin = new Microsoft.Maps.Pushpin(centerPoint, {
+    title: "Tartu Ülikool",
+    //subTitle: 'Hea koht',
+    //text: 'UT'
+  });
 
+  map.entities.push(pushpin);
 }
 
 // https://dev.virtualearth.net/REST/v1/Locations?q=1000 Vin Scully Ave, Los Angeles,CA&key=YOUR_KEY_HERE
-
